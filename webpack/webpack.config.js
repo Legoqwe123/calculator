fe16d2eef92874fae278c5a55253de0e69fff4b6
Kamalov Artur <prturkamalov@yandex.ru>
2020-11-05 17:04:38 +0400

    add cross platfrom enviroments for scripts

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,16 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin") // Require  html-webpack-plugin plugin
 const WebpackBar = require("webpackbar")
 const ESLintPlugin = require("eslint-webpack-plugin")
+const FriendlyErrorsWebpackPlugin =  require('friendly-errors-webpack-plugin') 
+const { path: ROOT_PATH } = require("app-root-path");
+
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: __dirname + "/src/main.ts", // webpack entry point. Module to start building dependency graph
+  entry: ROOT_PATH + "/src/main.ts", // webpack entry point. Module to start building dependency graph
   output: {
-    path: __dirname + "/dist", // Folder to store generated bundle
+    path: ROOT_PATH + "/dist", // Folder to store generated bundle
     filename: "bundle.js", // Name of generated bundle after build
     publicPath: "./", // public URL of the output directory when referenced in a browser
   },
@@ -30,10 +33,11 @@ module.exports = {
   plugins: [
     // Array of plugins to apply to build chunk
     new HtmlWebpackPlugin({
-      template: __dirname + "/index.html",
+      template: ROOT_PATH + "/index.html",
       inject: "body",
     }),
     new WebpackBar(),
+    new FriendlyErrorsWebpackPlugin(),
     new ESLintPlugin({
       context: './src',
       extensions: ['ts']
